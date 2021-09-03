0247cb59224d0deb31da076387dca4d10d5d9b2c
minhsuu14 <minhsuu14@gmail.com>
2021-09-03 18:39:20 +0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View, FlatList, Button } from 'react-native';
+import GoalInput from './components/GoalInput';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
+  const [goals, setGoals] = useState([]);
+  const [isAddNew, setIsAddNew] =useState(false);
+
+  const onPressButton = (enterGoal) => {
+      setGoals([...goals, {id : Math.random().toString(), value : enterGoal}]);
+      setIsAddNew(false);
+  }
+  const pressDelete = (goalId) => {
+    return setGoals((goals) => goals.filter(goal => {
+      return goal.id !== goalId
+    }));
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Button title="Add new Goal" onPress={()=> setIsAddNew(true)} />
+      <GoalInput onPressButton={onPressButton} visible={isAddNew} onPressCancel={()=>setIsAddNew(false)} />
+      <FlatList 
+        keyExtractor={goals.id}
+        data={goals} 
+        renderItem={(itemData)=>{
+          return(<GoalItem itemData={itemData} id={itemData.item.id} pressDelete={pressDelete}/>);
+        }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles = StyleSheet.create ({
+  screen: {
+    padding : 40
   },
-});
+})
